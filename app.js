123eec73216b90398d4bd680fcec194ca64dd298
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2022-12-25 17:16:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+//for writing isnide console
+// console.log("I am console");
+
+//for dispalying output on page
+// document.write("I will display on web page");
+
+// for popup window with OK buton
+// alert("I will popup with OK");
+
+//for anothe rpopup with OK and Cencel button
+// confirm("Another popup with Ok and cancel");
+
+//to take user input 
+// prompt("Enter value");
+
+
+//PRIMITIVE
+//var a=5;
+// var b=5;
+// document.writeln(a+b +"<br/>");
+
+// var x= "Mumbai";
+// document.writeln(x+"<br/>");
+
+// var p=true;
+// document.writeln(p+"<br/>");
+
+
+
+
+//NONPRIMITIVE
+//creation of an object in JS
+var car={
+    brand:"Toyota",
+    name:"Fortuner",
+    price:450000,
+    color:"Matt Black",
+
+
+
+};
+
+//printing object in js
+// to print onject on new line we will concatenate </br> tag 
+// document.writeln("My car brand is:- " + car.brand+"</br>");
+// document.writeln("My car name is:- " + car.name+"</br>");
+// document.writeln("My car price is:- " + car.price+"</br>");
+// document.writeln("My car color is:- " + car.color+"</br>");
+
+
+// How to create an array in JS 
+var car=["Fortuner","Maruti 800","Wagonr","Innova","Ferrari","BMW z5"]
+document.writeln(car[1]);
+document.writeln(car[4]);
+
+
+
+
+
+
+
